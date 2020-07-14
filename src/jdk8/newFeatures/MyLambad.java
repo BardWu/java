42894ede0ae62edbf68c13d21c07b65242d78efe
wu heng <1034250349@qq.com>
2020-07-14 18:06:18 +0800

    lambad test

--- src/jdk8/newFeatures/MyLambad.java
+++ src/jdk8/newFeatures/MyLambad.java
@@ -2,6 +2,8 @@ package jdk8.newFeatures;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.function.*;
+
 /**
  * @program: java
  * @description: lambad Test
@@ -13,11 +15,12 @@ public class MyLambad {
     //（）表示抽象方法的形参括号，{}表示抽象方法的实现。带着这种思想去学习
 
     @Test
-    //对于一个只有一个抽象方法的抽象类或者接口来说，抽象方法的实现就代表着创建这个类对象
+    //对于一个只有一个抽象方法的抽象类或者接口来说，抽象方法的实现就代表着创建这个类对象,也就是说在某种情况下
+    //lambda表达式可表示为一个对象
     //匿名内部类：不清楚类的具体名字，只关注其内部方法的实现
     public void test1(){
 
-        //创建对象方式1
+        //创建对象方式1 ->匿名内部内方式
         LambadInterface lambadInterface = new LambadInterface() {
             @Override
             public void MyTest(String a) {
@@ -27,10 +30,8 @@ public class MyLambad {
 
         lambadInterface.MyTest("123");
 
-        //创建对象方式2
-        LambadInterface lambadInterface2 = (a)->{
-            System.out.println(a);
-        };
+        //创建对象方式2 -> lambda方式
+        LambadInterface lambadInterface2 = (String a)-> System.out.println(a);;
 
         lambadInterface2.MyTest("123");
         // 结果一致
@@ -44,6 +45,56 @@ public class MyLambad {
         });
         thread.start();
     }
+
+    //jdk 提供的四种函数式接口
+    @Test
+    public void test3(){
+        //消费性
+        Consumer<String> consumer = (a)->{
+            System.out.println("consumer");
+        };
+        consumer.accept("2");
+
+        //供给性接口:
+        Supplier<Object> supplier = ()->{return  new Object();};
+        supplier.get();
+
+        //函数性接口
+        Function<Integer,String>  function = (i)->{
+            if(1==i){
+                return new String("a");
+            }else{
+                return new String("b");
+            }
+        };
+        function.apply(1);
+        //判断性
+        Predicate<String> predicate = (i)->{
+            if("a".equals(i)) {
+                return true;
+            }
+
+            return false;
+        };
+    }
+    @Test
+    public void test4(){
+    // String.valueOf();静态方法
+    // equals()：实例方法
+        Function<Integer,String>  function = (i)->{
+            return String.valueOf(i);
+        };
+
+        Function<Integer,String>  function2 = String::valueOf;
+
+        System.out.println(function2.apply(1));
+
+       Consumer<String> consumer = (i)->{System.out.println(i);};
+       consumer = (i)-> System.out.println(i);
+       consumer = System.out::println;
+
+
+    };
 
 
 
