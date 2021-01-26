f1b00400e9e0b6ffb8edfa515b475e8f00e89984
吴恒 <wuheng@tiza.com.cn>
2021-01-26 17:24:18 +0800

    git 冲突测试

---
+++ src/main/java/gitConflict/Test1.java
@@ -0,0 +1,17 @@
+package gitConflict;
+
+/**
+ * @program: java
+ * @description:
+ * @author: wh
+ * @create: 2021-01-26 17:22
+ **/
+public class Test1 {
+
+
+    private int age;
+
+    private String name;
+
+
+}
