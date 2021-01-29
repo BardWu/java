be0735e29665459b66cc8517c0e0571a42d052b0
吴恒 <wuheng@tiza.com.cn>
2021-01-29 11:21:21 +0800

    测试冲突

---
+++ src/main/java/gitConflict/Test2.java
@@ -0,0 +1,30 @@
+package gitConflict;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @program: java
+ * @description:
+ * @author: wh
+ * @create: 2021-01-29 11:20
+ **/
+public class Test2 {
+
+
+    private int age;
+
+    private String name;
+
+    private String sex;
+    private String address;
+
+
+    public static void main(String[] args) {
+        List<String> itemNameList = new ArrayList<>();
+        itemNameList.add("国五");
+        itemNameList.add("国六");
+        System.out.println(itemNameList);
+        System.out.println("aaa");
+    }
+}
