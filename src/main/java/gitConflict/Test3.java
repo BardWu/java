6338da88cf8b3520345824bad09b0d0763aff0bb
吴恒 <wuheng@tiza.com.cn>
2021-02-01 15:19:57 +0800

    测试冲突

---
+++ src/main/java/gitConflict/Test3.java
@@ -0,0 +1,31 @@
+package gitConflict;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @program: java
+ * @description:
+ * @author: wh
+ * @create: 2021-01-29 11:20
+ **/
+public class Test3 {
+
+
+    private int age;
+
+    private String name;
+
+    private String sex;
+    private String address;
+
+
+    public static void main(String[] args) {
+        List<String> itemNameList = new ArrayList<>();
+        itemNameList.add("国五");
+        itemNameList.add("国六");
+        System.out.println(itemNameList);
+        System.out.println("aaa");
+        System.out.println("dev branch");
+    }
+}
