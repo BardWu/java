e8e37fad22fbabd37996892809b0c78015ce0102
吴恒 <wuheng@tiza.com.cn>
2021-02-01 15:24:45 +0800

    测试冲突

--- src/main/java/gitConflict/Test3.java
+++ src/main/java/gitConflict/Test3.java
@@ -12,20 +12,5 @@ import java.util.List;
 public class Test3 {
 
 
-    private int age;
 
-    private String name;
-
-    private String sex;
-    private String address;
-
-
-    public static void main(String[] args) {
-        List<String> itemNameList = new ArrayList<>();
-        itemNameList.add("国五");
-        itemNameList.add("国六");
-        System.out.println(itemNameList);
-        System.out.println("aaa");
-        System.out.println("dev branch");
-    }
 }
